19c707fd1ca07a9becfb1e19163239c7a97294ce
Arif Kurniawan <52802394+arla27@users.noreply.github.com>
2022-11-18 11:09:34 +0700

    update

--- admin/save_voucher.php
+++ admin/save_voucher.php
@@ -13,12 +13,12 @@ if ($_SESSION["level"] != 1) {
 if (post("kode")) {
     $kode = post("kode");
     $nominal = post("nominal");
-    // $stats = utf8_encode(post("stats"));
+    $stats = post("stats");
     $u = $_SESSION['username'];
     $count = countNo("kode_voucher", "kode", $nominal, "make_by", $u);
     if ($count == 0) {
-        $q = mysqli_query($koneksi, "INSERT INTO kode_voucher(`kode`, `nominal`, `make_by`)
-            VALUES('$kode', '$nominal', '$u')");
+        $q = mysqli_query($koneksi, "INSERT INTO kode_voucher(`kode`, `nominal`,`stats`, `make_by`)
+            VALUES('$kode', '$nominal','not used', '$u')");
         swal_set("success", "Berhasil menambahkan voucher");
         redirect("save_voucher.php");
     } else {
@@ -71,33 +71,36 @@ require_once('../templates/header.php');
             </div>
         </div>
         <!-- End Page Header -->
-        <div class="alert alert-danger alert-dissmissible fade show" role="alert">
+        <!-- <div class="alert alert-danger alert-dissmissible fade show" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
             <div class="text-center">Import Kontak langsung dengan Excel?</div>
             <div class="text-center"><a href="<?= $base_url; ?>lib/template-import-kontak.xlsx" class="btn btn-success text-center">Download Template</a></div>
-        </div>
+        </div> -->
         <!-- Default -->
         <div class="row container">
             <div class="col">
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                     <i class="ion ion-person-add"></i>Add voucher
-                </button>
+                </button> -->
             </div>
+            <!-- <a class="btn btn-info float-right" href="<?= $base_url; ?>lib/kontakgroup.php" style="margin:5px">Export Contact Group</a> -->
+            <a class="btn btn-info float-right" href="<?= $base_url; ?>lib/voucher.php" style="margin:5px">Export Voucher Excel</a>
         </div>
+
+
         <div class="mb-4"></div>
         <div class="row">
             <div class="col-xl-12">
                 <!-- Sorting -->
                 <div class="widget has-shadow">
-                    <div class="widget-header bordered no-actions d-flex align-items-center">
+                    <!-- <div class="widget-header bordered no-actions d-flex align-items-center">
                         <div class="col"> Saved voucher</div>
                         <a class="btn btn-info float-right" href="<?= $base_url; ?>lib/export_excel.php" style="margin:5px"><i class="ion ion-aperture"></i>Export Excel</a>
-                        <button type="button" class="btn btn-success float-right" data-toggle="modal" style="margin:5px" data-target="#import"><i class="ion 
-ion-archive"></i>
+                        <button type="button" class="btn btn-success float-right" data-toggle="modal" style="margin:5px" data-target="#import"><i class="ion ion-archive"></i>
                             Import Excel
                         </button>
                         <a class="btn btn-danger float-right" href="save_voucher.php?act=delete_all" style="margin:5px"><i class="ion ion-trash-a"></i> Delete All</a>
-                    </div>
+                    </div> -->
 
                     <div class="widget-body">
                         <div class="table-responsive">
@@ -105,11 +108,11 @@ ion-archive"></i>
                                 <thead>
                                     <tr>
                                         <th>ID</th>
-                                        <th>Kode Voucher</th>
+                                        <!-- <th>Kode Voucher</th> -->
                                         <th>Nominal</th>
                                         <th>Status</th>
                                         <th>Date Used</th>
-                                        <th>Action</th>
+                                        <!-- <th>Action</th> -->
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -122,7 +125,7 @@ ion-archive"></i>
                                         $id++;
                                         echo '<tr>';
                                         echo '<td>' . $id . '</td>';
-                                        echo '<td>' . $row['kode'] . '</td>';                                        
+                                        // echo '<td>' . $row['kode'] . '</td>';                                        
                                         echo '<td>' .'Rp. '.number_format($row['nominal'],0,"",".")  . '</td>';
                                         // echo '<td>' . $row['stats'] . '</td>';                                        
                                         if (strlen(utf8_decode($row['stats'])) >= 50) {
@@ -131,7 +134,7 @@ ion-archive"></i>
                                             echo '<td>' . utf8_decode($row['stats']) . '</td>';
                                         }
                                         echo '<td>' . $row['date_used'] . '</td>';
-                                        echo '<td class="td-actions"><a href="#" data-toggle="modal" data-target="#Update_' . $row['id'] . '"><i class="la la-edit edit"></i></a><a href="save_voucher.php?act=hapus&id=' . $row['id'] . '"><i class="la la-close delete"></i></a></td>';
+                                        // echo '<td class="td-actions"><a href="#" data-toggle="modal" data-target="#Update_' . $row['id'] . '"><i class="la la-edit edit"></i></a><a href="save_voucher.php?act=hapus&id=' . $row['id'] . '"><i class="la la-close delete"></i></a></td>';
                                         echo '</tr>';
                                     }
 
@@ -142,6 +145,64 @@ ion-archive"></i>
                     </div>
                 </div>
                 <!-- End Sorting -->
+            </div>
+        </div>
+        <div class="row flex-row">
+            <!-- Begin Facebook -->
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <div class="widget widget-12 has-shadow">
+                    <div class="widget-body">
+                        <div class="media">
+                            <div class="align-self-center ml-5 mr-5">
+                                <i class="ion-checkmark-circled text-success"></i>
+                            </div>
+                            <div class="media-body align-self-center">
+                                <div class="title text-facebook">Penerima</div>
+                                <div class="number"> <?= countDB("user", "make_by", $username) ?> dari <?= countDB("kode_voucher",  "make_by", "admin") ?> Total Voucher</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <div class="widget widget-12 has-shadow">
+                    <div class="widget-body">
+                        <div class="media">
+                            <div class="align-self-center ml-5 mr-5">
+                                <i class="ion-close-circled text-danger"></i>
+                            </div>
+                            <div class="media-body align-self-center">
+                                <div class="title text-facebook">Belum digunakan</div>
+                                <div class="number"><?= countUS("kode_voucher", "stats", "not used", "make_by", "admin") ?> Voucher</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-6 col-sm-6">
+                <div class="widget widget-12 has-shadow">
+                    <div class="widget-body">
+                        <div class="media">
+                            <div class="align-self-center ml-5 mr-5">
+                                <i class="ion-pie-graph text-facebook"></i>
+                            </div>
+                            <div class="media-body align-self-center">
+                                <div class="title text-facebook">Persentase</div>
+                                <div class="progress progress-lg mb-3">
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated <?php
+                                                                                                        if (countPresentase() < 40) {
+                                                                                                            echo "bg-danger";
+                                                                                                        } else if (countPresentase() <= 70) {
+                                                                                                            echo "bg-warning";
+                                                                                                        } else {
+                                                                                                            echo "bg-success";
+                                                                                                        } ?>" role="progressbar" style="width: <?= countPresentase() ?>%" aria-valuenow="<?= countPresentase() ?>" aria-valuemin="0" aria-valuemax="100"><?= countPresentase() ?>%</div>
+                                </div>
+                                <!--<div class="number"></div>-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
