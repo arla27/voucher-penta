fab405e77f4dca39ad268b9c589add96e5ec44c5
arif kurniawan <arifkurniawan.ak5@gmail.com>
2022-11-18 10:56:29 +0700

    delete2

--- users/hapus_pesan.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../helper/conn.php");
-include_once("../helper/function.php");
-
-
-$id = get("id");
-
-$q = mysqli_query($koneksi, "DELETE FROM pesan WHERE id='$id'");
-redirect("broadcast.php");
