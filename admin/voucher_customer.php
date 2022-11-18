144c6b3a03acaad87a75d788f1069bef7a365c19
Arif Kurniawan <52802394+arla27@users.noreply.github.com>
2022-11-18 11:07:35 +0700

    update

--- admin/voucher_customer.php
+++ admin/voucher_customer.php
@@ -274,17 +274,7 @@ require_once('../templates/header.php');
                 });
             }
 
-            // function validasiEmail() {
-	        //     var rs = document.forms["formInput"]["email"].value;
-	        //     var atps=rs.indexOf("@");
-	        //     var dots=rs.lastIndexOf(".");
-	        // if (atps<1 || dots<atps+2 || dots+2>=rs.length) {
-		    //     alert("Alamat email tidak valid.");
-		    // return false;
-	        // } else {
-		    //     alert("Alamat email valid.");
-	        // }
-            // }
+
         </script>
 
     </body>
